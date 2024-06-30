1d7828f150aca02a5a3bf42bfd2dd98dfd5d3cad
Hariharan <101561549+harimuruga@users.noreply.github.com>
2024-06-30 17:30:41 +0530

    Initial commit

---
+++ component/banner/launchingSoonHero.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { toggleVideoOpen } from "@/redux/features/videoSlice";
+import { useAppDispatch } from "@/redux/hooks";
+import Link from "next/link";
+import React from "react";
+
+const BannerSection = () => {
+  const dispatch = useAppDispatch();
+  const handleVideoShow = () => {
+    dispatch(toggleVideoOpen());
+  };
+  return (
+    <section className="tf__banner">
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-7 col-lg-8">
+            <div className="tf__banner_text wow fadeInUp">
+              {/* <h5></h5> */}
+              <h1>
+                Welcome to <span>Grey Matter Solutions</span> Website Launching
+                Soon!
+              </h1>
+              <p>The place where eduction become adventure</p>
+              {/* <ul className="d-flex flex-wrap align-items-center">
+                <li>
+                  <Link className="common_btn" href="/about">
+                    Read More
+                  </Link>
+                </li>
+                <li>
+                  <a
+                    className="venobox play_btn"
+                    role="button"
+                    onClick={handleVideoShow}
+                  >
+                    <i className="fas fa-play"></i>
+                  </a>
+                </li>
+              </ul> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BannerSection;

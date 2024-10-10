891dbb338274c3f3459ac6f6d8b69366f79179e4
harivel1212 <120641281+harivel1212@users.noreply.github.com>
2024-10-10 18:11:35 +0530

    Revert "update"
    This reverts commit f73da0c4162e276962ee3ee63d2cf9150007a54e.

--- components/layout/header/Header2.js
+++ components/layout/header/Header2.js
@@ -80,7 +80,7 @@ export default function Header2({
                              View result
                               </Link>
                               
-                            
+                              
                               
                             </li>
                           </ul>
